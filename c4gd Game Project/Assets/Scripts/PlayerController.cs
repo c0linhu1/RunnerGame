8b5aca438a0abb59010109a399e9e96d05c4e85e
S1uuuuuu <zhoujierui114514@outlook.com>
2023-08-08 13:34:32 -0400

    Speed Boost when touching purple pad

--- c4gd Game Project/Assets/Scripts/PlayerController.cs
+++ c4gd Game Project/Assets/Scripts/PlayerController.cs
@@ -7,24 +7,31 @@ public class PlayerController : MonoBehaviour
 {
     
     public float speed = 40;
-    
 
     private float forwardInput;
     private float horizontalInput;
 
+    public float wallSlideSpeedMultiplier = 2f;
+
     private Rigidbody rb; 
     public float jumpForce = 180;
-    public float doublejumpForce = 10;
+    public float doublejumpForce = 100  ;
 
     public bool isOnGround = true;
     public bool firstjump = false;
     public bool doublejump = false;
     
     public bool wallslide = false;
+    private bool isWallSliding = false;
 
+    public bool wallSlideGravityEnabled = false;
 
     public AudioSource jump;
-  //  public AudioSource jump2;
+
+    public float accelerationForce = 2500f;
+
+    public bool isPushingForward = false;
+    // public AudioSource jump2;
 
     // public bool started = false;
     
@@ -46,9 +53,11 @@ public class PlayerController : MonoBehaviour
         // transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed); 
         // transform.Translate(forwardInput * Vector3.forward * Time.deltaTime * speed);
 
-        Vector3 nextV = new Vector3(horizontalInput * speed, rb.velocity.y, forwardInput * speed);
-        //print(nextV);
-        rb.velocity = nextV;
+        if (!isPushingForward) {
+            Vector3 nextV = new Vector3(horizontalInput * speed, rb.velocity.y, forwardInput * speed);
+            //print(nextV);
+            rb.velocity = nextV;
+        }
 
         // if(started) {
         //     transform.Translate(Vector3.forward * Time.deltaTime * speed);
@@ -56,23 +65,33 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space)) {
             if (isOnGround) {
-            jump = GetComponent<AudioSource>();
-            jump.Play();
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isOnGround = false;
-            firstjump = true;
-  
+                // jump = GetComponent<AudioSource>();
+                // jump.Play();
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                isOnGround = false;
+                firstjump = true;
             } else if (firstjump) {
                 rb.AddForce(Vector3.up * doublejumpForce, ForceMode.Impulse);
-                jump.Play();
+                // jump.Play();
                 firstjump = false;
                 doublejump = true;
             }
         }
 
-        if (!wallslide) {
-            rb.useGravity = true;
-        }
+    //    if (wallslide && !isOnGround)
+    //     {
+    //         isWallSliding = true;
+    //         rb.useGravity = wallSlideGravityEnabled;
+    //         if (isWallSliding)
+    //         {
+    //             rb.velocity = new Vector2(rb.velocity.x, -speed * wallSlideSpeedMultiplier);
+    //         }
+    //     }
+    //     else
+    //     {
+    //         isWallSliding = false;
+    //         rb.useGravity = true;
+    //     }
 
         // if (transform.position.x <= 8.0f || transform.position.x >= -8.0f) {
         //     wallslide = false;
@@ -82,6 +101,7 @@ public class PlayerController : MonoBehaviour
         //     rb.useGravity = false;
         // }
     }
+
 
     private void OnCollisionEnter(Collision other) {
         if (other.gameObject.CompareTag("Ground")) {
@@ -98,17 +118,19 @@ public class PlayerController : MonoBehaviour
         else if (other.gameObject.CompareTag("Wall")) {
             wallslide = true;
             // attachedRigidbody.useGravity = false;
-            speed = 35;
+            speed = 80;
+            rb.useGravity = false;
         }
         else if (other.gameObject.CompareTag("BounceUp")) {
             rb.useGravity = true;
-            rb.AddForce(Vector3.up * 300, ForceMode.Impulse);
+            rb.AddForce(Vector3.up * 250, ForceMode.Impulse);
             firstjump = false;
             doublejump = false;
         }
-        // else if (other.gameObject.CompareTag("PushForward")) {
-        //     transform.Translate(horizontalInput * 10 * Vector3.forward);
-        // }
+        else if (other.gameObject.CompareTag("PushForward")) {
+            isPushingForward = true;
+            rb.AddForce(transform.forward * accelerationForce, ForceMode.Impulse);
+        }
         else if (other.gameObject.CompareTag("RotateObstacle")) {
             print("Game Over");
         }
@@ -127,7 +149,17 @@ public class PlayerController : MonoBehaviour
     //         wallslide = false;
     //     } 
     // }
-
+    
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            wallslide = false;
+            rb.useGravity = true;
+        } else if (other.gameObject.CompareTag("PushForward")) {
+            isPushingForward = false;
+        }
+    }
 }
 
 
